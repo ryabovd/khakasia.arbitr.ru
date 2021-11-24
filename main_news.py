c009a75a2fa79ea1146b138375f7b384269a71c7
Dmitry Ryabov <ryabovd@outlook.com>
2021-11-24 19:39:31 +0700

    Fix func order. Simplification func get_news_from_content.

--- main_news.py
+++ main_news.py
@@ -7,15 +7,29 @@ import datetime
 
 
 CSV = 'news_khakasia.arbitr.ru.csv'
-news_list_dict = []
 HOST = 'https://khakasia.arbitr.ru'
 URL = 'https://khakasia.arbitr.ru/?theme=courts_cecutient'
-
-
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     }
+
+def main():
+    last_date = get_last_date()
+    html = get_html(URL, params='')
+    soup = get_soup(html.text)
+    current_date = get_current_date(soup)
+    if dates_diff(last_date, current_date) == True:
+        content = get_content_news(soup)
+        news = get_news_from_content(content, last_date)
+        save_news(news, CSV)
+        subject = "Новости арбитражного суда Республики Хакасия на"
+        text = text_for_send(news)
+        send_notification(text, subject)
+        new_last_date = news[0]['news_date']
+        write_last_data_json(new_last_date)
+    else:
+        print('\nНовости ОТСУТСТВУЮТ\n')
 
 def get_last_date():
     with open('main_news_settings.json', 'r', encoding='utf-8') as file:
@@ -31,11 +45,24 @@ def get_soup(request_url):
     soup = BeautifulSoup(request_url, 'html.parser')
     return soup
 
+def get_current_date(soup):
+    current_date = soup.find('h6', class_="b-news-date").get_text().strip()
+    return current_date
+
+def convert_date(date):
+    date_list = date.split('.')
+    year, month, day = int(date_list[2]), int(date_list[1]), int(date_list[0])
+    return datetime.date(year, month, day)
+
+def dates_diff(last_date, current_date):
+    return convert_date(current_date) > convert_date(last_date)
+
 def get_content_news(soup):
     items = soup.find_all('div', class_ = "b-news-content-wrapper")
     return items
 
-def get_news_from_content(items, news_list_dict, last_date):
+def get_news_from_content(items, last_date):
+    news_list_dict = []
     for item in items:
         date_new = item.find('h6', class_="b-news-date").get_text().strip()
         if convert_date(last_date) < convert_date(date_new):
@@ -82,35 +109,6 @@ def write_last_data_json(new_last_date):
         data = {}
         data['last_date'] = new_last_date
         json.dump(data, file)
-
-def get_current_date(soup):
-    current_date = soup.find('h6', class_="b-news-date").get_text().strip()
-    return current_date
-
-def convert_date(date):
-    date_list = date.split('.')
-    year, month, day = int(date_list[2]), int(date_list[1]), int(date_list[0])
-    return datetime.date(year, month, day)
-
-def dates_diff(last_date, current_date):
-    return convert_date(current_date) > convert_date(last_date)
-
-def main():
-    last_date = get_last_date()
-    html = get_html(URL, params='')
-    soup = get_soup(html.text)
-    current_date = get_current_date(soup)
-    if dates_diff(last_date, current_date) == True:
-        content = get_content_news(soup)
-        news = get_news_from_content(content, news_list_dict, last_date)
-        save_news(news, CSV)
-        subject = "Новости арбитражного суда Республики Хакасия на"
-        text = text_for_send(news)
-        send_notification(text, subject)
-        new_last_date = news[0]['news_date']
-        write_last_data_json(new_last_date)
-    else:
-        print('Новостей нет')
 
 
 if __name__ == "__main__":
